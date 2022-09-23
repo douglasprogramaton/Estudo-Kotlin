6a0fa0f7e33dd98a3dcc7ce6e3203efc1640810f
douglasprogramaton <douglassantosprogramaton@gmail.com>
2022-09-23 18:02:22 -0300

    Update

--- mercado-livro/src/main/kotlin/com/mercadolivro/repository/CustomerQueryNative.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/repository/CustomerQueryNative.kt
@@ -3,18 +3,20 @@ package com.mercadolivro.repository
 import com.mercadolivro.model.CustomerModel
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface CustomerQueryNative: JpaRepository<CustomerModel, Int> {
 
-   @Query("SELECT * FROM name WHERE customer = ?1", nativeQuery = true)
-   fun  findCustomer(customerModel: CustomerModel){
-      var lista = listOf<CustomerModel>()
-
+   @Query(
+      value = "SELECT * FROM customer WHERE id = :userId ",
+      nativeQuery = true
+   )
+   fun listByCustomerId(@Param("userId") userId: String): CustomerModel
 
    }
 
 
 
-}
+
 
 
