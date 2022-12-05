8fb91ae14e9f4ae2b8bba5817eb613552ae9a7d6
douglasprogramaton <douglassantosprogramaton@gmail.com>
2022-12-05 07:42:28 -0300

    Estudo API

---
+++ rest-whit-spring-boot-and-kotlin/src/main/kotlin/com/kotlinstping/restwhitspringbootandkotlin/RestWhitSpringBootAndKotlinApplication.kt
@@ -0,0 +1,12 @@
+package com.kotlinstping.restwhitspringbootandkotlin
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWhitSpringBootAndKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWhitSpringBootAndKotlinApplication>(*args)
+}
